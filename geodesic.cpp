bc04dcc8957b9f8003d4bbf5ad780e0311bd7c1d
William Tracy <afishionado@gmail.com>
2015-06-23 16:23:26 -0700

    New algorithm that does not generate redundant points

--- geodesic.cpp
+++ geodesic.cpp
@@ -1,23 +1,60 @@
 #include <stdio.h>
 #include <vector>
 #include <assert.h>
-#include <SDL.h>
+#include <time.h>
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include <GL/glu.h>
+#include <SDL.h>
 #include "vector3d.h"
 
-struct Face {
-  int vA;
-  int vB;
-  int vC;
 
-  //Face(int aa, int ab, int ac): a(aa), b(ab), c(ac) {}
+struct Edge {
+  int a;
+  int b;
+  int center;
+
+  int childA;
+  int childB;
+
+  void set(int aIn, int bIn) {
+    a = aIn;
+    b = bIn;
+    center = -1;
+  }
+
+  int split(std::vector<vector3d_t>& vertices, std::vector<Edge>& edges) {
+    if (center == -1) {
+      vector3d_t v(0, 0, 0);
+      Edge edge;
+      v += vertices[a];
+      v += vertices[b];
+      v.normalize();
+      vertices.push_back(v);
+      center = vertices.size() - 1;
+
+
+      edge.set(a, center);
+      edges.push_back(edge);
+      childA = edges.size() - 1;
+
+      edge.set(center, b);
+      edges.push_back(edge);
+      childB = edges.size() - 1;
+    }
+    return center;
+  }
+};
+
+struct Face {
+  int ab;
+  int bc;
+  int ca;
 
   void set(int a, int b, int c) {
-    vA = a;
-    vB = b;
-    vC = c;
+    ab = a;
+    bc = b;
+    ca = c;
   }
 };
 
@@ -37,47 +74,62 @@ void printGLError(GLenum error) {
 }
 
 
-int enqueue(std::vector<vector3d_t> &vertices, vector3d_t vector) {
-  vertices.push_back(vector);
-  return vertices.size() - 1;
-}
 
 
-void makeFace(int a, int b, int c, std::vector<Face> &faces) {
-  Face face;
-  face.set(a, b, c);
-  faces.push_back(face);
-}
 
+void subdivide(Face& face, std::vector<vector3d_t> &vertices, std::vector<Edge>& edges, std::vector<Face> &faces, int count) {
+  if (count > 0) {
+    Edge temp;
 
-void subdivide(int a, int b, int c, std::vector<vector3d_t> &vertices, std::vector<Face> &faces, int count) {
-  vector3d_t vector(0, 0, 0);
+    temp = edges[face.ab];
+    int d = temp.split(vertices, edges);
+    edges[face.ab] = temp;
+    int ad = edges[face.ab].childA;
+    int db = edges[face.ab].childB;
 
-  vector = vertices[a];
-  vector += vertices[b];
-  vector.normalize();
-  int d = enqueue(vertices, vector);
+    temp = edges[face.bc];
+    int e = temp.split(vertices, edges);
+    edges[face.bc] = temp;
+    int be = edges[face.bc].childA;
+    int ec = edges[face.bc].childB;
 
-  vector = vertices[b];
-  vector += vertices[c];
-  vector.normalize();
-  int e = enqueue(vertices, vector);
+    temp = edges[face.ca];
+    int f = temp.split(vertices, edges);
+    edges[face.ca] = temp;
+    int cf = edges[face.ca].childA;
+    int fa = edges[face.ca].childB;
 
-  vector = vertices[c];
-  vector += vertices[a];
-  vector.normalize();
-  int f = enqueue(vertices, vector);
-  
-  if (count <= 0) {
-    makeFace(a, d, f, faces);
-    makeFace(b, d, e, faces);
-    makeFace(c, e, f, faces);
-    makeFace(d, e, f, faces);
-  } else {
-    subdivide(a, d, f, vertices, faces, count - 1);
-    subdivide(b, d, e, vertices, faces, count - 1);
-    subdivide(c, e, f, vertices, faces, count - 1);
-    subdivide(d, e, f, vertices, faces, count - 1);
+    Edge edge;
+
+    edge.set(d, e);
+    edges.push_back(edge);
+    int de = edges.size() - 1;
+
+    edge.set(e, f);
+    edges.push_back(edge);
+    int ef = edges.size() - 1;
+
+    edge.set(f, d);
+    edges.push_back(edge);
+    int fd = edges.size() - 1;
+
+    Face face;
+
+    face.set(ad, fd, fa);
+    faces.push_back(face);
+    subdivide(face, vertices, edges, faces, count - 1);
+
+    face.set(be, de, db);
+    faces.push_back(face);
+    subdivide(face, vertices, edges, faces, count - 1);
+
+    face.set(cf, ef, ec);
+    faces.push_back(face);
+    subdivide(face, vertices, edges, faces, count - 1);
+
+    face.set(de, ef, fd);
+    faces.push_back(face);
+    subdivide(face, vertices, edges, faces, count - 1);
   }
 }
 
@@ -88,31 +140,20 @@ int main(int argc, char**argv) {
   assert(sizeof(Face) == 3 * sizeof(GLint));
 
   std::vector<vector3d_t> vertices;
+  std::vector<Edge> edges;
   std::vector<Face> faces;
   bool stillGoing = true;
 
-  /* initialize SDL */
   SDL_Init(SDL_INIT_VIDEO);
-
-  /* set the title bar */
   SDL_WM_SetCaption("Geodesic", "Geodesic");
-
-  /* create window */
   SDL_Surface* screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_OPENGL);
-
-  glClearColor(1, 1, 1, 1);
-
+  glClearColor(0, 0, 0, 1);
   glViewport(0, 0, 640, 480);
-
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-
-  glOrtho(-1, 1, 1, -1, 1, -1);
-
+  glOrtho(-2, 2, 2, -2, 1, -1);
   glMatrixMode(GL_MODELVIEW);
-
   glEnable(GL_TEXTURE_2D);
-
   glLoadIdentity();
 
   // generate mesh
@@ -126,41 +167,52 @@ int main(int argc, char**argv) {
   vector.set(0, 1, 0);
   vertices.push_back(vector);
 
-  subdivide(0, 1, 3, vertices, faces, 1);
-  subdivide(1, 2, 3, vertices, faces, 1);
-  subdivide(2, 0, 3, vertices, faces, 1);
+  Edge edge;
+  edge.set(0, 1);
+  edges.push_back(edge);
+  edge.set(3, 0);
+  edges.push_back(edge);
+  edge.set(1, 2);
+  edges.push_back(edge);
+  edge.set(3, 1);
+  edges.push_back(edge);
+  edge.set(2, 0);
+  edges.push_back(edge);
+  edge.set(2, 3);
+  edges.push_back(edge);
 
+  int subdivisions = 6;
+  Face face;
+  face.set(0, 1, 3);
+  faces.push_back(face);
+  face.set(2, 3, 5);
+  faces.push_back(face);
+  face.set(4, 5, 1);
+  faces.push_back(face);
+
+  subdivide(faces[0], vertices, edges, faces, subdivisions);
+  subdivide(faces[1], vertices, edges, faces, subdivisions);
+  subdivide(faces[2], vertices, edges, faces, subdivisions);
+
+  printf("Vertex count: %d\n", vertices.size());
   
-  for (int i = 0; i < vertices.size(); ++i) {
-    vector = vertices[i];
-  }
-  for (int i = 0; i < faces.size(); ++i) {
-    Face face;
-    face = faces[i];
-  }
-
-  GLfloat vertexBuffer[] = {0, 1, 1.0, -1, -1, 1.0, 1, -1, 1.0};
-
   while (stillGoing) {
-
     SDL_Event event;
     SDL_PollEvent(&event);
     if (event.type == SDL_QUIT)
       stillGoing = false;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3f(0, 0, 0);
+    glColor3f(0, 1, 0);
     glPushMatrix();
     glLoadIdentity();
+    //glRotatef(clock() / 1000, 1, 0, 0);
     glRotatef(90, 1, 0, 0);
 
     glEnableClientState(GL_VERTEX_ARRAY);
 
     glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
-    printGLError(glGetError());
-
-    glDrawElements(GL_LINES, faces.size() * 3, GL_UNSIGNED_INT, &faces[0]);
-    printGLError(glGetError());
+    glDrawArrays(GL_POINTS, 0, vertices.size() - 0);
 
     glPopMatrix();
     glDisableClientState(GL_VERTEX_ARRAY);
