e7ca8394b5a45a6aa4ca64cfc209b2a0e70d37ff
William Tracy <afishionado@gmail.com>
2015-06-23 11:21:56 -0700

    OpenGL port.

--- geodesic.cpp
+++ geodesic.cpp
@@ -1,20 +1,40 @@
 #include <stdio.h>
 #include <vector>
+#include <assert.h>
+#include <SDL.h>
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GL/glu.h>
 #include "vector3d.h"
 
 struct Face {
-  int a;
-  int b;
-  int c;
+  int vA;
+  int vB;
+  int vC;
 
-  Face(int aa, int ab, int ac): a(aa), b(ab), c(ac) {}
+  //Face(int aa, int ab, int ac): a(aa), b(ab), c(ac) {}
 
-  void set(int aa, int ab, int ac) {
-    a = aa;
-    b = ab;
-    c = ac;
+  void set(int a, int b, int c) {
+    vA = a;
+    vB = b;
+    vC = c;
   }
 };
+
+void printGLError(GLenum error) {
+  switch(error) {
+    case GL_NO_ERROR: 
+      break;
+    case GL_INVALID_ENUM:
+      fprintf(stderr, "Invalid enum\n");
+      break;
+    case GL_INVALID_VALUE:
+      fprintf(stderr, "Invalid value\n");
+      break;
+    default: 
+      fprintf(stderr, "Unknown GL error\n");
+  }
+}
 
 
 int enqueue(std::vector<vector3d_t> &vertices, vector3d_t vector) {
@@ -24,7 +44,8 @@ int enqueue(std::vector<vector3d_t> &vertices, vector3d_t vector) {
 
 
 void makeFace(int a, int b, int c, std::vector<Face> &faces) {
-  Face face(a, b, c);
+  Face face;
+  face.set(a, b, c);
   faces.push_back(face);
 }
 
@@ -62,16 +83,37 @@ void subdivide(int a, int b, int c, std::vector<vector3d_t> &vertices, std::vect
 
 
 int main(int argc, char**argv) {
+  assert(sizeof(PFLOAT) == sizeof(GLfloat));
+  assert(sizeof(vector3d_t) == 3 * sizeof(GLfloat));
+  assert(sizeof(Face) == 3 * sizeof(GLint));
+
   std::vector<vector3d_t> vertices;
   std::vector<Face> faces;
+  bool stillGoing = true;
 
-  printf("<?xml version=\"1.0\"?>\n<scene type=\"triangle\">");
-  printf("<material name=\"Yellow.001\"><color r=\"0.859948\" g=\"0.841801\" b=\"0.185443\" a=\"1\"/><type sval=\"shinydiffusemat\"/></material>");
-  printf("<light name=\"Lamp.001\"><color r=\"1\" g=\"1\" b=\"1\" a=\"1\"/><corner x=\"-0.25\" y=\"02.25\" z=\"2\"/><from x=\"0\" y=\"02.5\" z=\"2\"/><point1 x=\"-0.25\" y=\"02.75\" z=\"2\"/><point2 x=\"0.25\" y=\"02.25\" z=\"2\"/><power fval=\"5\"/><type sval=\"arealight\"/></light>");
-  printf("<camera name=\"cam\"><from x=\"0\" y=\"3.9\" z=\"0\"/><resx ival=\"375\"/><resy ival=\"375\"/><to x=\"0\" y=\"3.7\" z=\"0\"/><type sval=\"perspective\"/><up x=\"0\" y=\"-3.9\" z=\"2\"/></camera>");
-  printf("<integrator name=\"default\"><type sval=\"photonmapping\"/></integrator>");
-  printf("<integrator name=\"volintegr\"><type sval=\"none\"/></integrator>");
-  printf("<render><AA_inc_samples ival=\"2\"/><AA_minsamples ival=\"4\"/><AA_passes ival=\"2\"/><AA_pixelwidth fval=\"1.5\"/><AA_threshold fval=\"0.05\"/><camera_name sval=\"cam\"/><filter_type sval=\"mitchell\"/><gamma fval=\"2.2\"/><height ival=\"375\"/><integrator_name sval=\"default\"/><volintegrator_name sval=\"volintegr\"/><width ival=\"375\"/></render>");
+  /* initialize SDL */
+  SDL_Init(SDL_INIT_VIDEO);
+
+  /* set the title bar */
+  SDL_WM_SetCaption("Geodesic", "Geodesic");
+
+  /* create window */
+  SDL_Surface* screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_OPENGL);
+
+  glClearColor(1, 1, 1, 1);
+
+  glViewport(0, 0, 640, 480);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  glOrtho(-1, 1, 1, -1, 1, -1);
+
+  glMatrixMode(GL_MODELVIEW);
+
+  glEnable(GL_TEXTURE_2D);
+
+  glLoadIdentity();
 
   // generate mesh
   vector3d_t vector;
@@ -84,22 +126,48 @@ int main(int argc, char**argv) {
   vector.set(0, 1, 0);
   vertices.push_back(vector);
 
-  subdivide(0, 1, 3, vertices, faces, 2);
-  subdivide(1, 2, 3, vertices, faces, 2);
-  subdivide(2, 0, 3, vertices, faces, 2);
+  subdivide(0, 1, 3, vertices, faces, 1);
+  subdivide(1, 2, 3, vertices, faces, 1);
+  subdivide(2, 0, 3, vertices, faces, 1);
+
   
-  printf("<mesh vertices=\"%d\" faces=\"%d\">", vertices.size(), faces.size());
   for (int i = 0; i < vertices.size(); ++i) {
     vector = vertices[i];
-    printf("<p x=\"%f\" y=\"%f\" z=\"%f\" />", vector.x, vector.y, vector.z);
   }
-  printf("<set_material sval=\"Yellow.001\"/>");
   for (int i = 0; i < faces.size(); ++i) {
-    Face face(0, 0, 0);
+    Face face;
     face = faces[i];
-    printf("<f a=\"%d\" b=\"%d\" c=\"%d\"/>", face.a, face.b, face.c);
   }
-  printf("</mesh>");
 
-  printf("</scene>");
+  GLfloat vertexBuffer[] = {0, 1, 1.0, -1, -1, 1.0, 1, -1, 1.0};
+
+  while (stillGoing) {
+
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT)
+      stillGoing = false;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glLoadIdentity();
+    glRotatef(90, 1, 0, 0);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
+    printGLError(glGetError());
+
+    glDrawElements(GL_LINES, faces.size() * 3, GL_UNSIGNED_INT, &faces[0]);
+    printGLError(glGetError());
+
+    glPopMatrix();
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+    SDL_GL_SwapBuffers();
+  }
+
+  SDL_Quit();
+  return 0;
 }
